fbfc41c8a4d504375f3e4d2ca1f8aa6deb8709a0
aldojunior990 <aldojunior990@gmail.com>
2021-10-07 13:46:47 -0300

    creating a subscribe button component

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export function SubscribeButton() {
+  return (
+    <button className={styles.subscribeButton} type="button">
+      Subscribe now
+    </button>
+  );
+}
