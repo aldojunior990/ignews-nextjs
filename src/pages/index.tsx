75f0d1a3697a8bd7205033dac0c1486d6b2f2f8c
aldojunior990 <aldojunior990@gmail.com>
2021-10-13 12:51:10 -0300

    add static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { stripe } from "../services/stripe";
 
 interface HomeProps {
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1Jhzt7BvVhpQvJ1PfBEFZ97v");
   const product = {
     priceId: price.id,
@@ -52,5 +52,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24,
   };
 };
